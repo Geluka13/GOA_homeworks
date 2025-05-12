237bc3ed7b2d2974e7b9d77c83c0922029363f53
Geluka13 <gelukagamer2000@gmail.com>
2025-05-13 00:45:58 +0400

    'commit'

---
+++ day 70/CW/cw1.py
@@ -0,0 +1,10 @@
+class Human:
+    def __init__(self, name, surname, age):
+        self.name = name
+        self.surname = surname
+        self.age = age
+    
+human1 = Human("Nika", "Samkharadze", 15)
+print(human1.name)
+print(human1.surname)
+print(human1.age)
